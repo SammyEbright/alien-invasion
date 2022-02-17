94b7b58c2d2f9490f439d0f1bbfa59c72df52beb
Sammy <samuel.ebright@gmail.com>
2022-02-16 18:25:26 -0800

    Modify alien_invasion.py to store bullets in a group.

--- alien_invasion.py
+++ alien_invasion.py
@@ -24,6 +24,7 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
         # Set the background color
         self.bg_color = (50, 150, 230)
@@ -34,6 +35,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
+            self.bullets.update()
             self._update_screen()
 
     def _check_events(self):
