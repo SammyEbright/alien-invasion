89f234ce76b267fdcd0eb351a66df9051555fc43
Sammy <samuel.ebright@gmail.com>
2022-02-18 09:00:32 -0800

    Modify alien_invasion.py to fire, delete, limit bullets.

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from bullets import Bullet
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -35,7 +36,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
+            self._update_bullet()
             self._update_screen()
 
     def _check_events(self):
@@ -56,6 +57,8 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
 
     def _check_keyup_events(self, event):
         if event.key == pygame.K_RIGHT:
@@ -63,10 +66,29 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
 
+    def _fire_bullet(self):
+        """Create a new bullet and add it to the bullets group."""
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullet(self):
+        """Update the position of bullets and get rid of old bullets."""
+        # Update bullet positions.
+        self.bullets.update()
+
+        # Get rid of bullets that have disappeared.
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+        # print(len(self.bullets))
+
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
 
         pygame.display.flip()
         
--- settings.py
+++ settings.py
@@ -14,6 +14,7 @@ class Settings:
         # Bullet settings
         self.bullet_speed = 1.0
         self.bullet_width = 3
-        self.bullet_height = 15
-        self.bullet_color = (60, 60, 60)
+        self.bullet_height = 7.5
+        self.bullet_color = (100, 0, 0)
+        self.bullets_allowed = 3
         
