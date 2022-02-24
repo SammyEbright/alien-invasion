05af45b2eea747cf889be36518ccba6e0acd6a79
Sammy <samuel.ebright@gmail.com>
2022-02-23 20:07:39 -0800

    Add practices/practice-12-4.py.

---
+++ practices/practice-12-4.py
@@ -0,0 +1,122 @@
+import sys
+
+import pygame
+
+class Game:
+    """Overall class to manage game assets and behavior."""
+
+    def __init__(self) -> None:
+        """Initialize the game, and create game resources."""
+        pygame.init()
+
+        # self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        pygame.display.set_caption("Rocket Game")
+
+        self.rocket = Rocket(self)
+
+        # Set the background color.
+        self.bg_color = (0, 150, 225)
+
+    def run_game(self):
+        """Start the main loop for the game."""
+        while True:
+            self._check_events()
+            self.rocket.update()
+            self._update_screen()
+
+            # Make the most recently drawn screen visible.
+            pygame.display.flip()
+
+    def _check_events(self):
+        """Respond to keypresses and mouse events."""
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    self._check_keydown_events(event)
+                elif event.type == pygame.KEYUP:
+                    self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.rocket.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.rocket.moving_left = True
+        elif event.key == pygame.K_UP:
+            self.rocket.moving_up = True
+        elif event.key == pygame.K_DOWN:
+            self.rocket.moving_down = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+        # elif event.key == pygame.K_SPACE:
+        #     self._fire_bullet()
+
+    def _check_keyup_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.rocket.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.rocket.moving_left = False
+        elif event.key == pygame.K_UP:
+            self.rocket.moving_up = False
+        elif event.key == pygame.K_DOWN:
+            self.rocket.moving_down = False
+
+    def _update_screen(self):
+        """Update images on the screen and flip to the new screen."""
+        self.screen.fill(self.bg_color)
+        self.rocket.blitme()
+
+
+class Rocket:
+    """A class to manage the rocket."""
+    
+    def __init__(self, my_game) -> None:
+        """Initialize the rocket and set its starting position."""
+        self.screen = my_game.screen
+        self.screen_rect = my_game.screen.get_rect()
+
+        # Load the rocket image and get its rect.
+        self.image = pygame.image.load('images/rocket.bmp')
+        self.rect = self.image.get_rect()
+
+        # Start each new rocket at the center of the screen.
+        self.rect.center = self.screen_rect.center
+
+        # Rocket speed
+        self.rocket_speed = 1.5
+
+        # Store a decimal value for the rocket's positions.
+        self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
+
+        # Movement flag
+        self.moving_right = False
+        self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
+
+    def update(self):
+        """Update the rocket's position based on the movement flag."""
+        # Update the rocket's x and y values not the rect:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.rocket_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.rocket_speed
+        if self.moving_up and self.rect.top > 0:
+            self.y -= self.rocket_speed
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.y += self.rocket_speed
+
+        self.rect.x = self.x
+        self.rect.y = self.y
+
+    def blitme(self):
+        """Draw the rocket at its current location."""
+        self.screen.blit(self.image, self.rect)
+
+
+if __name__ == '__main__':
+    # Make a game instance, and run the game.
+    game = Game()
+    game.run_game()
